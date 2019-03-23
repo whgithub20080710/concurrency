b97d705199c05f5a64d76a3e1c6ac2a289e105c2
woodenstick <309136832@qq.com>
2019-03-23 16:06:29 +0800

    增加并发模拟

---
+++ src/main/java/com/wooden/concurrency/TestController.java
@@ -0,0 +1,18 @@
+package com.wooden.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        log.info("test on "+System.currentTimeMillis());
+        return "test";
+    }
+}
