5f4b6dc7e4b56764ba7ee1d835ad98665fa4af29
woodenstick <309136832@qq.com>
2019-03-23 12:58:20 +0800

    添加标识注解

---
+++ src/main/java/com/wooden/concurrency/annoations/Recommend.java
@@ -0,0 +1,16 @@
+package com.wooden.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记推荐的类或写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+}
